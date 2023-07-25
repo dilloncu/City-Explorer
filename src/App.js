4f4d89ad9a1a7a3dad7389a50d675f0ab58729a0
Dillon Cushman <83660406+dilloncu@users.noreply.github.com>
2023-07-25 09:40:34 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState({});
+  const [map, setMap] = useState("");
+
+  function handleSearch(event) {
+    setSearchQuery(event.target.value);
+  }
+
+  async function getLocation(event) {
+    try {
+      event.preventDefault();
+      event.target.input.value = "";
+      
+      const API = `https://eu1.locationiq.com/v1/search?key=${process.env.REACT_APP_API_KEY}&q=${searchQuery}&format=json`;
+      const res = await axios.get(API);
+      
+      setLocation(res.data[0]);
+      handleMap(res.data[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function handleMap(data) {
+    const API = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${data.lat},${data.lon}&zoom=9`;
+    setMap(API);
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={getLocation}>
+        <input type="text" placeholder="search for a city" name="input" onChange={handleSearch} />
+        <button type="submit">Explore!</button>
+      </form>
+      <p>{location.display_name}</p>
+      {map && <img src={map} alt="map" />}
+    </div>
+  );
+}
+
+export default App;
